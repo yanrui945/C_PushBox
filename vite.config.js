02dabf5d7da6e36d7a7afbc7c807483cf151e8a9
yanrui945 <yanrui945@163.com>
2025-03-09 19:57:24 +0800

    修复提交代码后，部署未与本地同步问题

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/C_PushBox/',  // 添加base配置，值为仓库名称
+  base: '/C_PushBox/',
+  build: {
+    outDir: 'docs'
+  },
   server: {
-    host: '0.0.0.0',  // 监听所有地址
-    port: 5173,       // 指定端口
+    host: '0.0.0.0',
+    port: 5173,
   },
   resolve: {
     alias: {
